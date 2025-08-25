cb6f3371d2191fa428e76913b2eae802f3732a9a
aluisioalonso <aluisio.alonso@academico.ifpb.edu.br>
2025-08-25 18:56:52 -0500

    alteração no frontend

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print('oi')
+
     return render_template('homepage.html')
 
 @app.route('/botao')
--- static/js/homepage.js
+++
@@ -1,8 +0,0 @@
-var linkdescricao = document.getElementById("cliqueca");
-var grandona = document.getElementById("grandona");
-
-function mostrar(){
-    grandona.style.backgroundColor = "red";
-}
-
-cliqueca.addEventListener('click', mostrar );
